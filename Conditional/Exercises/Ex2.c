64d66db71d41e5a860c9b8b2e4bb7fdd7251fe2d
Henryque02 <henryque.affiune10@gmail.com>
2025-05-06 09:54:44 -0300

    att

---
+++ Conditional/Exercises/Ex2.c
@@ -0,0 +1,12 @@
+/*2) Escreva um programa em C que lê 5 números inteiros, um por vez. Conte quantos destes valores
+são negativos e quantos são positivos. Ao final, imprima na tela a quantidade de números negativos
+e positivos.*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    
+    return 0;
+}
--- Conditional/relational_operators.c
+++ Conditional/relational_operators.c
@@ -31,11 +31,37 @@ else
 
 switch (expression)
 {
-case constant expression:
+case 1:
     code
     break;
 
-default:
+case 2:
+    code
     break;
 }
+
+
+printf("a - Cadastrar produto\nb - Vender produto\nc - Buscar produto\nd - Imprimir relatório\ne - Sair");
+scanf("%c", &opcao);
+
+switch(opcao){
+   case 'a':
+      // seu código para o caso 1
+      break;
+   case 'b':
+      // seu código para o caso 2
+      break;
+   case 'c':
+      // seu código para o caso 3
+      break;
+   case 'd':
+      // seu código para o caso 4
+      break;
+   case 'e':
+      // seu código para o caso 5
+      break;
+   default:
+      // para informar uma opção inválida
+}
+
 */
