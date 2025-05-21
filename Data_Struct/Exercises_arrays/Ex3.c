e7904cf8e688af7d5b1dc66dc7a701f6bd1e5a83
Henryque02 <henryque.affiune10@gmail.com>
2025-05-21 09:14:00 -0300

    att

--- Data_Struct/Exercises_arrays/Ex3.c
+++ Data_Struct/Exercises_arrays/Ex3.c
@@ -6,7 +6,7 @@ imprima a soma de todos os elementos de uma matriz 5 x 7.
 #include<stdio.h>
 
 int main(){
-    int matriz[2][3] = {1,2,3,4,5,6}, soma = 0;
+    int matriz[2][3] = {1,2,3,4,5,7}, soma = 0;
     for (int i = 0; i < 2; i++)
     {
         for (int  j = 0; j < 3; j++)
