01967ecc8a2b85f100bce20c339690b730a88a17
Henryque02 <henryque.affiune10@gmail.com>
2025-05-23 10:24:47 -0300

    att

---
+++ Data_Struct/String/End_of_string.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    char palavras[50] = {"Hi. let's learn to program"};
+    
+    return 0;
+}
--- Data_Struct/String/Ways_to_get_string.c
+++ Data_Struct/String/Ways_to_get_string.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 
 int main(){
-    char palavras[] = {"Hi. let's learn to program"};
+    char palavras[50] = {"Hi. let's learn to program"};
+    
     printf("Digite seu nome: ");
-    scanf("%10[^\n]", palavras); //[^\n] faz o scanf ler ate que o enter seja digitado e 10 ajusta o tamnho do vetor
-    printf("%s", palavras);
+
+    //scanf("%10[^\n]", palavras); [^\n] faz o scanf ler ate que o enter seja digitado e 10 ajusta o tamnho do vetor
+    
+    fgets(palavras, 50, stdin); //ultimo caracter não aparece
+    printf("\n%s", palavras);
     return 0;
-} 
+}
