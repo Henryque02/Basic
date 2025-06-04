82fc366fd90389fcd7fb891bdd3569bd54847a63
Henryque02 <henryque.affiune10@gmail.com>
2025-06-04 09:11:33 -0300

    att

--- Data_Struct/String/Exercise/Ex1.c
+++ Data_Struct/String/Exercise/Ex1.c
@@ -1,23 +1,41 @@
-/*Como descobrir se uma palavra é palíndroma*/
+/*
+Como descobrir se uma palavra é palíndroma
+  Asa
+  arara
+  ama
+  osso
+  radar
+  socos
+  reviver
+  */
 
 #include<stdio.h>
 #include<string.h>
 
 int main()
 {
-    char palavra[20];
-    printf("Digite uma palavra: ");
-    scanf("%s20[^\n]", palavra);
-    int tamanho = strlen(palavra), inverso = tamanho;
-    for (int i = 0; i < tamanho; i++)
-    {
-        if (palavra[i] != palavra[inverso - 1])
-        {
-            printf("A palavra nao e palindroma!");
-        }
-        tamanho --;
-    }
-    printf("A palavra %s e palindroma.", palavra);
-    return 0;
-}
+  char palavra[20];
 
+  printf("Digite uma palavra: ");
+  scanf("%19s", palavra);
+
+  int tamanho = strlen(palavra), palindroma = 1;
+
+  for (int i = 0; i < tamanho/2; i++)
+  {
+    if (tolower(palavra[i]) != tolower(palavra[tamanho - 1 - i]))
+    {
+      palindroma = 0;
+      break;
+    }
+  }
+
+  if (palindroma)
+  {
+    printf("A palavra e palindroma!");
+  }
+  else
+    printf("A palavra nao e palindroma!");
+
+  return 0;
+}
--- Data_Struct/String/Exercise/Ex2.c
+++ Data_Struct/String/Exercise/Ex2.c
@@ -1,10 +1,38 @@
-/*Como descobrir se uma frase é palíndroma*/
+/*
+Como descobrir se uma frase é palíndroma
+luz azul
+anotaram a data da maratona
+roma me tem amor
+a grama é amarga
+a mãe te ama
+*/
 
 #include<stdio.h>
 #include<string.h>
 
 int main()
 {
-    
+    char frase[50];
+    int tamanho = 0, palindroma = 1;
+
+    printf("Digite uma frase: ");
+    scanf("%49s[^\n]", frase);
+    tamanho = strlen(frase);
+    for (int i = 0; i < tamanho/2; i++)
+    {
+        if (frase[i] != frase[tamanho - 1 - i])
+        {
+            palindroma = 0;
+            break;
+        }
+    }
+    if (palindroma)
+    {
+        printf("A frase e palindroma");
+    }
+    else
+        printf("A frase nao e palindroma!");
+
     return 0;
 }
+
