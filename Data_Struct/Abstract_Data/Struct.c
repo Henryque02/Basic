70416c0596592e417c49cd1b48299471fc7c17ad
Henryque02 <henryque.affiune10@gmail.com>
2025-06-11 08:37:36 -0300

    att

--- Data_Struct/Abstract_Data/Struct.c
+++ Data_Struct/Abstract_Data/Struct.c
@@ -1,28 +1,46 @@
 #include<stdio.h>
+#include<string.h>
+
+typedef struct
+{
+    int dia, mes, ano;
+}DataNascimento;
+
 
 typedef struct
     {
         int idade;
         char sexo;
         char nome[100];
+        DataNascimento;
     }Pessoa;
     
+    /*
     struct Pessoa2
     {
         int idade;
         char sexo;
         char nome[100];
-    };
+    };*/
 
 int main(){
     Pessoa pessoa1;
-    struct Pessoa2 pessoa2;
+    //struct Pessoa2 pessoa2;
 
+    /*
     pessoa1.idade = 35;
     pessoa1.sexo = 'f';
-    pessoa1.nome = "Maria";   
-    
-    printf("Nome: %s\nIdade: %d\nSexo: %c\n", pessoa1.nome, pessoa1.idade, pessoa1.sexo);
+    strcpy(pessoa1.nome, "Maria");   
+    */
+   printf("Digite seu nome: ");
+   fgets(pessoa1.nome, 100, stdin);
+   printf("Digite sua idade: ");
+   scanf(" %d",&pessoa1.idade);
+   printf("Digite seu sexo: ");
+   scanf(" %c",&pessoa1.sexo);
+   printf("Digite sua data de nascimento: ");
+   scanf("  %d %d %d",&pessoa1.dia,&pessoa1.mes, &pessoa1.ano);
+   printf("\n\nNome: %sIdade: %d\nSexo: %c\nData de Nascimento: %02d/%02d/%d", pessoa1.nome, pessoa1.idade, pessoa1.sexo, pessoa1.dia, pessoa1.mes, pessoa1.ano);
 
     return 0;
 }
---
+++ Data_Struct/Abstract_Data/a.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    int base;
+
+    printf("Insira o tamanho da base: ");
+    scanf("%d", &base);
+    
+    for (int i = 0; i < base; i++)
+    {
+        printf("\n")
+        for (int j = 0; j < base; j++)
+        {
+            d
+        }
+        
+    }
+    
+    
+    return 0;
+}
