ad306242600186d732647110c67cc812d0ffc1c6
Henryque02 <henryque.affiune10@gmail.com>
2025-06-17 13:58:39 -0300

    att

--- Projetos/test.c
+++ Projetos/test.c
@@ -6,15 +6,7 @@ int main()
     printf("Digite o tamanho da base: ");
     scanf("%d", &base);
 
-    for (i = 1; i < base+1; i++)
-    {
-        printf("*");
-    }
-    for (i = base; i > 0; i--)
-    {
-        printf("\n");
-    }
-    
+    //*
     
     return 0;
 }
