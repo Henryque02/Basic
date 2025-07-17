5f1da3846bb9aeec51f7c31c6499a5b495e35ca0
Henryque02 <henryque.affiune10@gmail.com>
2025-07-17 07:11:35 -0300

    att

--- Projetos/Jogo_da_Velha.c
+++ Projetos/Jogo_da_Velha.c
@@ -3,18 +3,38 @@
 
     Objetivo: fazer um jogo da velha
 */
+
 #include<stdio.h>
+
+void imprimir_Matriz(int matriz[3][3])
+{
+    for (size_t i = 0; i < count; i++)
+    {
+        for (size_t i = 0; i < count; i++)
+        {
+            /* code */
+        }
+        
+    }
+    
+}
 
 int main()
 {
-    int Quero_jogar;
-    printf("Bem vindo ao meu jogo da velha!\n\nQuer jogar uma partida agora?\n");
-    scanf("%d", &Quero_jogar);
-    while (Quero_jogar != 1 || Quero_jogar != 2)
+    int jogar = 0, matriz[3][3];
+    printf("\tVamos jogar uma partida?\n\n1- Sim\n2- Nao\n\nDigite o numero da opcao: ");
+    scanf(" %d", &jogar);
+    while (jogar != 1 && jogar != 2)
     {
-        printf("Digite uma opcao valida: ");
-        scanf("%d", &Quero_jogar);
+        printf("Opcao invalida! Digite uma opcao valida: ");
+        scanf(" %d", &jogar);
     }
+    if (jogar == 2)
+    {
+        printf("\nTudo bem! Jogamos outra hora (;");
+        return 0;
+    }
+    printf("\nJogador 1 sera o X e o jogador 2 a O\n\n");
     
     return 0;
 }
